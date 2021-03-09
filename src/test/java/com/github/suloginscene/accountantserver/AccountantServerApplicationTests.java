f1ef3eb4c64d4c4e3e254315f8246ff78f80214d
suloginscene <suloginscene@gmail.com>
2021-03-09 22:00:17 +0900

    feat: Initialize

---
+++ src/test/java/com/github/suloginscene/accountantserver/AccountantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.suloginscene.accountantserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountantServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
