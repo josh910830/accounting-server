f2e5d79cc0d86d28106e5dab629427e68e72f55c
suloginscene <suloginscene@gmail.com>
2021-03-11 03:25:57 +0900

    feat: Add change command stock balance

--- src/main/java/com/github/suloginscene/accountant/account/Stock.java
+++ src/main/java/com/github/suloginscene/accountant/account/Stock.java
@@ -2,6 +2,7 @@ package com.github.suloginscene.accountant.account;
 
 import com.github.suloginscene.accountant.common.holder.Holder;
 import com.github.suloginscene.accountant.common.money.Money;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.AttributeOverride;
@@ -16,12 +17,21 @@ import static lombok.AccessLevel.PROTECTED;
 public abstract class Stock extends Account {
 
     @AttributeOverride(name = "amount", column = @Column(name = "balance"))
+    @Getter
     private Money balance;
 
 
     protected Stock(Holder holder, String name, Money balance) {
         super(holder, name);
         this.balance = balance;
+    }
+
+    public void increase(Money amount) {
+        balance = Money.add(balance, amount);
+    }
+
+    public void decrease(Money amount) {
+        balance = Money.subtract(balance, amount);
     }
 
 }
---
+++ src/test/java/com/github/suloginscene/accountant/account/StockTest.java
@@ -0,0 +1,60 @@
+package com.github.suloginscene.accountant.account;
+
+import com.github.suloginscene.accountant.common.holder.Holder;
+import com.github.suloginscene.accountant.common.money.Money;
+import com.github.suloginscene.accountant.common.money.NegativeMoneyException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static com.github.suloginscene.accountant.account.AccountType.ASSET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@DisplayName("저량 계정")
+class StockTest {
+
+    Stock stock;
+
+
+    @BeforeEach
+    void setup() {
+        Holder holder = new Holder(1L);
+        String name = "국민 예금";
+        Money base = Money.of(1);
+        AccountCreationParameter param = new AccountCreationParameter(ASSET, holder, name, base);
+        Account account = Account.create(param);
+        stock = (Stock) account;
+    }
+
+
+    @Test
+    @DisplayName("증가 정상 - 잔고 증가")
+    void increase_onSuccess_increasesBalance() {
+        Money money = Money.of(1);
+        stock.increase(money);
+
+        assertThat(stock.getBalance()).isEqualTo(Money.of(2));
+    }
+
+    @Test
+    @DisplayName("감소 정상 - 잔고 감소")
+    void decrease_onSuccess_increasesBalance() {
+        Money money = Money.of(1);
+        stock.decrease(money);
+
+        assertThat(stock.getBalance()).isEqualTo(Money.of(0));
+    }
+
+    @Test
+    @DisplayName("감소 음수 - 예외 발생")
+    void decrease_onFail_increasesBalance() {
+        Money money = Money.of(2);
+        Executable action = () -> stock.decrease(money);
+
+        assertThrows(NegativeMoneyException.class, action);
+    }
+
+}
