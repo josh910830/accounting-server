c2eec6b4a7d215c9068fe4d6f92c0442ce0f742b
suloginscene <suloginscene@gmail.com>
2021-04-19 06:36:01 +0900

    chore: Refine properties for deploy

--- src/main/java/com/github/suloginscene/accountant/App.java
+++ src/main/java/com/github/suloginscene/accountant/App.java
@@ -9,7 +9,8 @@ public class App {
     private static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.properties,"
             + "file:/accountant/application-db.properties,"
-            + "file:/accountant/application-jwt.properties";
+            + "file:/accountant/application-jwt.properties,"
+            + "file:/accountant/application-mail.properties";
 
 
     public static void main(String[] args) {
